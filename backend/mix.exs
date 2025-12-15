16156ece3d95816beb652493d895b230aa0bcc5f
Leo Lee <leoohylee@gmail.com>
2025-12-15 23:02:05 +0000

    missing exs fix

---
+++ backend/mix.exs
@@ -0,0 +1,77 @@
+defmodule DragnCards.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :dragncards,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {DragnCards.Application, []},
+      extra_applications: [:logger, :runtime_tools, :mnesia]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      {:phoenix, "~> 1.6.11"},
+      {:phoenix_pubsub, "~> 2.0.0"},
+      {:phoenix_ecto, "~> 4.0"},
+      {:ecto_sql, "~> 3.4.2"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:gettext, "~> 0.22"},
+      {:jason, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"},
+      {:pow, "~> 1.0.19"},
+      {:swoosh, "~> 0.25.1"},
+      {:parent, "~> 0.9.0"},
+      {:accessible, "~> 0.2.1"},
+      {:ex_check, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 1.3.1", only: :dev, runtime: false},
+      {:dialyxir, ">= 1.0.0", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:sobelow, ">= 0.0.0", only: :dev, runtime: false},
+      {:map_diff, "~> 1.3"},
+      {:erlport, "~> 0.10.1"},
+      {:hackney, "~> 1.9"},
+      {:poison, "~> 5.0"},
+      {:luerl, github: "rvirding/luerl"},
+      {:httpoison, "~> 1.8"}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+    ]
+  end
+end
